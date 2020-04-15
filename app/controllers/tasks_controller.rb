ba7b61ae5232b0468610a23207427a1c5b39f6ac
David Osuna Azcona Dominguez <david.oad@gmail.com>
2020-04-14 20:37:06 -0600

    final app

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -1,15 +1,60 @@
 class TasksController < ApplicationController
+  # GET '/articles'
   def index
     @tasks = Task.all
   end
 
+  # GET '/artciles/:id'
   def show
-    @article = Article.find(params[:id])
+    @task = Task.find(params[:id])
   end
 
+  # POST '/articles/new'
   def new
+    @task = Task.new
   end
 
+  # POST '/tasks/'
+  def create
+    @task = Task.new(article_params)
+    @task.save
+    redirect_to @task
+  end
+
+  # GET '/tasks/:id/edit'
   def edit
+    @task = Task.find(params[:id])
+  end
+
+  # PATCH '/tasks/:id'
+  def update
+    @task = Task.find(params[:id])
+    @task.update(article_params)
+    redirect_to @task
+  end
+
+  # PATCH '/tasks/:id'
+
+  def completed_status
+    @task = Task.find(params[:completed])
+  end
+
+  def mark_as_done
+    @task = Task.find(params[:id])
+    @task.update_attribute(:completed, true)
+    redirect_to @task, notice: 'Completed task'
+  end
+
+  # DELETE '/tasks/:id'
+  def destroy
+    @task = Task.find(params[:id])
+    @task.destroy
+    redirect_to tasks_path
+  end
+
+  private
+
+  def article_params
+    params.require(:task).permit(:title, :details, :completed)
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,4 @@
 Rails.application.routes.draw do
-  get 'tasks/index'
-  get 'tasks/show'
-  get 'tasks/new'
-  get 'tasks/edit'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   get '/tasks', to: 'tasks#index'
 
@@ -14,5 +10,5 @@ Rails.application.routes.draw do
 
   get '/tasks/:id', to: 'tasks#show', as: 'task'
 
-  get '/tasks/:id', to: 'tasks#destroy'
+  delete '/tasks/:id', to: 'tasks#destroy'
 end
